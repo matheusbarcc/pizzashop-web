58e24d469385d2d04586a9c5ec1ca8f6d11b1430
matheusbarcc <matheusbarcon@gmail.com>
2024-12-31 15:23:56 -0300

    test: add restaurant creation and dashboard mocks

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -2,9 +2,25 @@ import { setupWorker } from 'msw/browser'
 
 import { env } from '@/env'
 
+import { GetDailyRevenueInPeriodMock } from './get-daily-revenue-in-period-mock'
+import { GetDayOrdersAmountMock } from './get-day-orders-amount-mock'
+import { GetMonthCanceledOrdersAmountMock } from './get-month-canceled-orders-amount-mock'
+import { GetMonthOrdersAmountMock } from './get-month-orders-amount-mock'
+import { GetMonthRevenueMock } from './get-month-revenue-mock'
+import { GetPopularProductsMock } from './get-popular-products-mock'
+import { registerRestaurantMock } from './register-restaurant-mock'
 import { signInMock } from './sign-in-mock'
 
-export const worker = setupWorker(signInMock)
+export const worker = setupWorker(
+  signInMock,
+  registerRestaurantMock,
+  GetDayOrdersAmountMock,
+  GetMonthOrdersAmountMock,
+  GetMonthCanceledOrdersAmountMock,
+  GetMonthRevenueMock,
+  GetDailyRevenueInPeriodMock,
+  GetPopularProductsMock,
+)
 
 export async function enableMSW() {
   if (env.MODE !== 'test') {
