3c580b46a714b9cde1c2dd039661fca6a301192e
matheusbarcc <matheusbarcon@gmail.com>
2025-06-06 15:22:54 -0300

    fix: app import in main tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import { enableMSW } from "./api/mocks";
-import { App } from "./app.tsx";
+import { App } from "./App";
 
 enableMSW().then(() => {
   createRoot(document.getElementById("root")!).render(
